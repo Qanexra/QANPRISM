ec13c1d89f0c11333183e91c24b6c2a26becf075
Qanexra <raymond@qanexra.com>
2026-08-13 02:29:42 +0700

    fix(ui): enable transparent window and transparent viewport layers so child webview is visible and interactable

--- src/components/TabPanel.jsx
+++ src/components/TabPanel.jsx
@@ -239,7 +239,8 @@ const TabPanel = ({ tab, isActive, onClose, onUpdateUrl, onUpdateTitle }) => {
           position: 'relative', 
           width: '100%', 
           height: '100%', 
-          backgroundColor: '#1e1e2e' 
+          backgroundColor: 'transparent',
+          pointerEvents: 'none'
         }}
       />
     </div>
